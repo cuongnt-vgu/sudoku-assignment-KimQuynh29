70e4cd1cbfbf34c71535d8df4491b15ac00e063f
Huynh Thi Kim Quynh <96537654+KimQuynh29@users.noreply.github.com>
2023-12-20 06:42:58 +0000

    change hidden single 4

--- src/hidden_singles.c
+++ src/hidden_singles.c
@@ -1,6 +1,7 @@
 #include "hidden_singles.h"
 #include "sudoku.h"
 #include <stddef.h>
+#include <stdlib.h>
 
 
 int find_hidden_single_values(Cell **p_cells, int *hidden_single_values) {
@@ -10,7 +11,7 @@ int find_hidden_single_values(Cell **p_cells, int *hidden_single_values) {
         int candidate_count = 0;
         Cell *hidden_single_cell = NULL;
 
-        for (int i = 0; i < BOARD_SIZE; ++i) {
+        for (int i = 0; i <= BOARD_SIZE; ++i) {
             if (p_cells[i]->num_candidates > 1 && is_candidate(p_cells[i], value)) {
                 ++candidate_count;
                 hidden_single_cell = p_cells[i];
@@ -19,8 +20,9 @@ int find_hidden_single_values(Cell **p_cells, int *hidden_single_values) {
 
         if (candidate_count == 1) {
             hidden_single_values[counter++] = value;
-            hidden_single_cell->value = value;
-            hidden_single_cell->num_candidates = 1;
+            /*chỉ mới tìm value thôi không gán vào cell*/
+            //hidden_single_cell->value = value;
+            //hidden_single_cell->num_candidates = 1;
         }
     }
 
@@ -29,36 +31,47 @@ int find_hidden_single_values(Cell **p_cells, int *hidden_single_values) {
 
 //find hidden single cells in a group (row, column, or box)
 void find_hidden_single(Cell **p_cells, HiddenSingle *p_hidden_singles, int *p_counter) {
-    int hidden_single_values[BOARD_SIZE];
+    int* hidden_single_values = malloc(sizeof(int)*BOARD_SIZE);
     int num_hidden_singles = find_hidden_single_values(p_cells, hidden_single_values);
 
     for (int i = 0; i < num_hidden_singles; ++i) {
-        p_hidden_singles[*p_counter].p_cell->value = hidden_single_values[i];
-        p_hidden_singles[*p_counter].p_cell->num_candidates = 1;
-        p_hidden_singles[*p_counter].value = hidden_single_values[i];
+        // find cell that contain hidden value
+        Cell *p_cell;
+
+        /*lỗi bị vòng lặp 2 lần nên in ra kq count=2 */
+        //p_hidden_singles[*p_counter].p_cell->value = hidden_single_values[i];
+        //p_hidden_singles[*p_counter].p_cell->num_candidates = 1;
+
+        /*tạo 1 vòng lặp để xét từng cell, khi gặp hidden value thì 
+        xác định hidden single value đó ở cell nào ở cell nào*/
+        p_hidden_singles[*p_counter] = {p_cell, value};
         (*p_counter)++;
     }
+
+    free(hidden_single_values);
 }
 
 // Function to find hidden single cells in a board, return the number of hidden single cells
 int hidden_singles(SudokuBoard *p_board) {
-    HiddenSingle hidden_singles[BOARD_SIZE * BOARD_SIZE];
+   HiddenSingle hidden_singles[BOARD_SIZE * BOARD_SIZE];
     int counter = 0;
+    
+    // Check hidden singles in each cell
+    for (int i = 0; i < BOARD_SIZE; ++i) {    
+    // Check hidden singles in each row
+        for (int k = 0; k < BOARD_SIZE; ++i) {
+            find_hidden_single(p_board->p_rows[i], hidden_singles, &counter);
+             // Check hidden singles in each column
+                for (int e = 0; e < BOARD_SIZE; ++i) {
+                    find_hidden_single(p_board->p_cols[i], hidden_singles, &counter);
+            // Check hidden singles in each box
+                    for (int f = 0; f < BOARD_SIZE; ++i) {
+                        find_hidden_single(p_board->p_boxes[i], hidden_singles, &counter);
+                                    }
+                                        }
+                                            }
 
-    // Check hidden singles in rows
-    for (int i = 0; i < BOARD_SIZE; ++i) {
-        find_hidden_single(p_board->p_rows[i], hidden_singles, &counter);
-    }
-
-    // Check hidden singles in columns
-    for (int i = 0; i < BOARD_SIZE; ++i) {
-        find_hidden_single(p_board->p_cols[i], hidden_singles, &counter);
-    }
-
-    // Check hidden singles in boxes
-    for (int i = 0; i < BOARD_SIZE; ++i) {
-        find_hidden_single(p_board->p_boxes[i], hidden_singles, &counter);
-    }
-
+                                                }
     return counter;
-}
+    }
+
--- src/naked_pairs.c
+++ src/naked_pairs.c
@@ -1,6 +1,78 @@
 #include "naked_pairs.h"
 
-int naked_pairs(SudokuBoard *p_board)
-{
-    return 0;
-}
+int naked_pairs(SudokuBoard* p_board) {return 0;}
+
+// int naked_pairs(SudokuBoard *p_board)
+// {
+//     int counter = 0;
+
+//     for (int i = 0; i < BOARD_SIZE; i++)
+//     {
+//         for (int j = 0; j < BOARD_SIZE; j++)
+//         {
+//             // find cells with two candidates
+//             if (p_board->data[i][j].num_candidates == 2)
+//             {
+//                 // check if other cells in the same row, column, or box have the same two candidates
+//                 int candidates[2] = {p_board->data[i][j].candidates[0], p_board->data[i][j].candidates[1]};
+//                 int other_cells[2] = {-1, -1};
+//                 for (int k = 0; k < BOARD_SIZE; k++)
+//                 {
+//                     if (k != j && p_board->data[i][k].num_candidates == 2 &&
+//                         p_board->data[i][k].candidates[0] == candidates[0] &&
+//                         p_board->data[i][k].candidates[1] == candidates[1])
+//                     {
+//                         other_cells[0] = k;
+//                         break;
+//                     }
+//                 }
+//                 for (int k = 0; k < BOARD_SIZE; k++)
+//                 {
+//                     if (k != i && p_board->data[k][j].num_candidates == 2 &&
+//                         p_board->data[k][j].candidates[0] == candidates[0] &&
+//                         p_board->data[k][j].candidates[1] == candidates[1])
+//                     {
+//                         other_cells[1] = k;
+//                         break;
+//                     }
+//                 }
+//                 for (int k = 0; k < 3; k++)
+//                 {
+//                     for (int l = 0; l < 3; l++)
+//                     {
+//                         if (i / 3 == k && j / 3 == l && p_board->p_boxes[k][l][0].num_candidates == 2 &&
+//                             p_board->p_boxes[k][l][0].candidates[0] == candidates[0] &&
+//                             p_board->p_boxes[k][l][0].candidates[1] == candidates[1])
+//                         {
+//                             other_cells[0] = p_board->p_boxes[k][l][1]->index;
+//                             other_cells[1] = p_board->p_boxes[k][l][2].index;
+//                             break;
+//                         }
+//                     }
+//                     if (other_cells[0] != -1 && other_cells[1] != -1)
+//                         break;
+//                 }
+
+//                 // if two cells are found, set the candidates to 0 for all other cells in the same row, column, or box
+//                 if (other_cells[0] != -1 && other_cells[1] != -1)
+//                 {
+//                     for (int k = 0; k < BOARD_SIZE; k++)
+//                     {
+//                         if (k != other_cells[0] && k != other_cells[1])
+//                         {
+//                             for (int l = 0; l < 2; l++)
+//                             {
+//                                 unset_candidate(&p_board->data[i][k], candidates[l]);
+//                                 unset_candidate(&p_board->data[k][j], candidates[l]);
+//                                 unset_candidate(&p_board->p_boxes[i / 3][j / 3][k], candidates[l]);
+//                             }
+//                         }
+//                     }
+//                     counter += 2;
+//                 }
+//             }
+//         }
+//     }
+
+//     return counter;
+// }
